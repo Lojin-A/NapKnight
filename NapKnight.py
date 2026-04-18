c876947bed9b43f5e722fe407d2efbf147f99d62
Lojin-A <lojin.ayesh1@gmail.com>
2026-04-18 17:22:03 +0300

    fixed the code

--- NapKnight.py
+++ NapKnight.py
@@ -4,7 +4,7 @@ import threading
 import winsound
 
 FRAME_LIMIT = 15     
-NO_FACE_LIMIT = 20   
+NO_FACE_LIMIT = 100   
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -27,12 +27,21 @@ class NapKnightApp:
 
         self.running = False
         self.driving_mode = False 
+        self.is_beeping = False
 
     def beep(self):
-        try:
-            winsound.Beep(2500, 500) 
-        except:
-            pass
+        if self.is_beeping:
+            return
+            
+        def play_sound():
+            self.is_beeping = True
+            try:
+                winsound.Beep(2500, 500) 
+            except:
+                pass
+            self.is_beeping = False
+
+        threading.Thread(target=play_sound).start()
 
     def start_system(self):
         if self.running: return
@@ -53,13 +62,15 @@ class NapKnightApp:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             key = cv2.waitKey(5) & 0xFF
             
-            if key == ord('q'):
+            if key == ord('q') or key == ord('Q'):
                 self.running = False
                 break
-            elif key == ord('d'):
+            elif key == ord('d') or key == ord('D'):
                 self.driving_mode = True
-            elif key == ord('p'):
+            elif key == ord('p') or key == ord('P'):
                 self.driving_mode = False
+                eye_counter = 0    
+                face_counter = 0
 
             cv2.rectangle(frame, (0,0), (frame.shape[1], 80), (0,0,0), -1)
 
@@ -70,7 +81,6 @@ class NapKnightApp:
             else:
                 cv2.putText(frame, "MODE: DRIVING", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-                # 1. FIND THE FACE
                 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
                 if len(faces) == 0:
@@ -83,7 +93,6 @@ class NapKnightApp:
                     (x, y, w, h) = faces[0]
                     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-                    # 2. FIND THE EYES
                     roi_gray = gray[y:y+h, x:x+w]
                     eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 10)
 
